4255472480de5ce0cdc87abc802d010792632cd3
Amobu0 <kdehgogo104@gmail.com>
2024-05-06 22:24:48 +0900

    24/5/6

---
+++ src/b5/BOJ2438.java
@@ -0,0 +1,18 @@
+package b5;
+
+import java.util.Scanner;
+
+public class BOJ2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+
+        for (int i = 0; i < N; i++) { // N번의 반복이 일어나야함
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
