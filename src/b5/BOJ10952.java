b721bc9adbff39c5ecf615b77fcb68d04d0ecd19
Amobu0 <kdehgogo104@gmail.com>
2024-05-07 22:13:52 +0900

    24/5/7 B3

---
+++ src/b5/BOJ10952.java
@@ -0,0 +1,20 @@
+package b5;
+
+import java.util.Scanner;
+
+public class BOJ10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            if (A + B == 0) {
+                return;
+            } else {
+                System.out.println(A + B);
+            }
+        }
+    }
+}
