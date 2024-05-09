f97ee81e8eca6980d5466d09d35ec408ef4207f9
Amobu0 <kdehgogo104@gmail.com>
2024-05-09 23:43:51 +0900

    24/5/9 B3

---
+++ src/b5/BOJ11654.java
@@ -0,0 +1,12 @@
+package b5;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        char c = text.charAt(0);
+        System.out.println((int)c);
+    }
+}
