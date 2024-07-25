518ddb3f96fa475af59070c8a2de12c2ffdc56b4
Amobu0 <kdehgogo104@gmail.com>
2024-07-25 14:30:16 +0900

    7/25_7

---
+++ src/bronze/b3/BOJ2442.java
@@ -0,0 +1,21 @@
+package bronze.b3;
+
+import java.util.Scanner;
+
+public class BOJ2442 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N + i; j++) {
+                if (N-i-j<=1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
