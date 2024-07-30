4ac61bccc923ca93bc83d796644c82044cb3db05
Amobu0 <kdehgogo104@gmail.com>
2024-07-30 23:10:33 +0900

    7/30_13

---
+++ src/bronze/b3/BOJ3053.java
@@ -0,0 +1,12 @@
+package bronze.b3;
+
+import java.util.Scanner;
+
+public class BOJ3053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        System.out.println(Math.PI*R*R);
+        System.out.println(2*R*R);
+    }
+}
